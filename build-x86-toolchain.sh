ad2264566d70597b4644ab6151a9fe2662e8b353
Neil Bostian <neil.bostian@gmail.com>
2020-05-08 15:06:51 -0700

    initial commit with dockerfile to build cross-compiler toolchain

---
+++ build-x86-toolchain.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+docker build . \
+    -f Dockerfile.toolchain \
+    -t toolchain:i686-elf \
+    --build-arg TARGET=i686-elf
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+rm -rf $DIR"/toolchain"
+
+ctr=$(docker create toolchain:i686-elf)
+
+docker cp $ctr:/toolchain $DIR/toolchain
+
+docker rm $ctr
